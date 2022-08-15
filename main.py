4d4f2d135ccd93be57efa383d86ca54c94c01006
Andrey Sozykin <sozykin@gmail.com>
2022-08-15 11:13:09 +0500

    Update main.pu

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ class Item(BaseModel):
     text: str
 
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
+classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Демонстрационное ИИ приложение для курса DevOps"}
 
 @app.post("/predict/")
 def predict(item: Item):
