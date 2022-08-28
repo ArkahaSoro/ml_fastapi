ca4263c3a06e2e02c8b45d5f46935536f74d1709
Andrey Sozykin <sozykin@gmail.com>
2022-08-28 08:10:59 +0500

    Add test_main.py

--- test_main.py
+++ test_main.py
@@ -7,11 +7,11 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Демонстрационное ИИ приложение для курса DevOps"}
 
 def test_predict_positive():
     response = client.post("/predict/",
-        json={"text": "I like machine learning!"}
+        json={"text": "Я люблю непрерывную интеграцию и тесты!"}
     )
     json_data = response.json() 
 
@@ -20,7 +20,7 @@ def test_predict_positive():
 
 def test_predict_negative():
     response = client.post("/predict/",
-        json={"text": "I hate machine learning!"}
+        json={"text": "Я ненавижу непрерывную интеграцию и тесты!"}
     )
     json_data = response.json() 
 
