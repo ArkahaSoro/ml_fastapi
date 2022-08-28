ef36c9c3540680f44e86907fa0210cc62089938a
Andrey Sozykin <sozykin@gmail.com>
2022-08-28 09:51:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     text: str
 
+
 app = FastAPI()
-classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+classifier = pipeline("sentiment-analysis",
+                      model="blanchefort/rubert-base-cased-sentiment")
+
 
 @app.get("/")
 def root():
     return {"message": "Демонстрационное ИИ приложение для курса DevOps"}
+
 
 @app.post("/predict/")
 def predict(item: Item):
